1bf3e2f2681ee430f76f3f6fe6b64845396ab31c
Nikita Sutulov <notsid212@gmail.com>
2024-10-10 11:32:48 +0300

    Initialized mod
    Added basic fabric mod project for the further developent.

---
+++ src/main/java/com/nickytoolchick/vanillaplus/VanillaPlusMod.java
@@ -0,0 +1,16 @@
+package com.nickytoolchick.vanillaplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaPlusMod implements ModInitializer {
+	public static final String MOD_ID = "vanillaplusmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/nickytoolchick/vanillaplus/VanillaPlusModClient.java
@@ -0,0 +1,10 @@
+package com.nickytoolchick.vanillaplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaPlusModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
