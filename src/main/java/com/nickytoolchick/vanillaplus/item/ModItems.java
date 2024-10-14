6fab3b4b882b17ca02d081a659e9e14ff15b1e33
Nikita Sutulov <notsid212@gmail.com>
2024-10-14 21:38:55 +0300

    Added daggers
    Added daggers, which are similar to swords, but although having less damage, they have higher attack speed.

--- src/main/java/com/nickytoolchick/vanillaplus/item/ModItems.java
+++ src/main/java/com/nickytoolchick/vanillaplus/item/ModItems.java
@@ -1,14 +1,46 @@
 package com.nickytoolchick.vanillaplus.item;
 
 import com.nickytoolchick.vanillaplus.VanillaPlusMod;
+import com.nickytoolchick.vanillaplus.item.custom.DaggerItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+    public static final Item WOODEN_DAGGER = registerItem(
+            "wooden_dagger",
+            new DaggerItem(ToolMaterials.WOOD, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.WOOD, 1, -1F)))
+    );
+
+    public static final Item STONE_DAGGER = registerItem(
+            "stone_dagger",
+            new DaggerItem(ToolMaterials.STONE, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.STONE, 1, -1F)))
+    );
+
+    public static final Item GOLDEN_DAGGER = registerItem(
+            "golden_dagger",
+            new DaggerItem(ToolMaterials.GOLD, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.GOLD, 1, -1F)))
+    );
+
+    public static final Item IRON_DAGGER = registerItem(
+            "iron_dagger",
+            new DaggerItem(ToolMaterials.IRON, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.IRON, 1, -1F)))
+    );
+
+    public static final Item DIAMOND_DAGGER = registerItem(
+            "diamond_dagger",
+            new DaggerItem(ToolMaterials.DIAMOND, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.DIAMOND, 1, -1F)))
+    );
+
+    public static final Item NETHERITE_DAGGER = registerItem(
+            "netherite_dagger",
+            new DaggerItem(ToolMaterials.NETHERITE, new Item.Settings().attributeModifiers(DaggerItem.createAttributeModifiers(ToolMaterials.NETHERITE, 1, -1F)))
+    );
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(VanillaPlusMod.MOD_ID, name), item);
@@ -16,5 +48,14 @@ public class ModItems {
 
     public static void registerModItems() {
         VanillaPlusMod.LOGGER.info("Registering Mod Items for " + VanillaPlusMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(WOODEN_DAGGER);
+            entries.add(STONE_DAGGER);
+            entries.add(GOLDEN_DAGGER);
+            entries.add(IRON_DAGGER);
+            entries.add(DIAMOND_DAGGER);
+            entries.add(NETHERITE_DAGGER);
+        });
     }
 }
---
+++ src/main/java/com/nickytoolchick/vanillaplus/item/custom/DaggerItem.java
@@ -0,0 +1,52 @@
+package com.nickytoolchick.vanillaplus.item.custom;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.component.type.ToolComponent;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.List;
+
+public class DaggerItem extends SwordItem {
+    public DaggerItem(ToolMaterial toolMaterial, Settings settings) {
+        super(toolMaterial, settings.component(DataComponentTypes.TOOL, createToolComponent()));
+    }
+
+    private static ToolComponent createToolComponent() {
+        return new ToolComponent(
+                List.of(
+                        ToolComponent.Rule.ofAlwaysDropping(List.of(Blocks.COBWEB), 30.0F),
+                        ToolComponent.Rule.of(BlockTags.SWORD_EFFICIENT, 3.0F)
+                ), 1.0F, 2
+        );
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return super.isEnchantable(stack);
+    }
+
+    public static AttributeModifiersComponent createAttributeModifiers(ToolMaterial material, int baseAttackDamage, float attackSpeed) {
+        return AttributeModifiersComponent.builder()
+                .add(
+                        EntityAttributes.GENERIC_ATTACK_DAMAGE,
+                        new EntityAttributeModifier(
+                                BASE_ATTACK_DAMAGE_MODIFIER_ID, baseAttackDamage + material.getAttackDamage(), EntityAttributeModifier.Operation.ADD_VALUE
+                        ),
+                        AttributeModifierSlot.MAINHAND
+                )
+                .add(
+                        EntityAttributes.GENERIC_ATTACK_SPEED,
+                        new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, attackSpeed, EntityAttributeModifier.Operation.ADD_VALUE),
+                        AttributeModifierSlot.MAINHAND
+                )
+                .build();
+    }
+}
